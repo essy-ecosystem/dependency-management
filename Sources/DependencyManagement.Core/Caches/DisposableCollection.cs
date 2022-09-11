f43ea218707ccf2405ed2367320c97c5cee6ad36
Romana F <fembina@pm.me>
2022-09-11 19:51:02 +0300

    Update disposable collection

--- Sources/DependencyManagement.Core/Caches/DisposableCollection.cs
+++ Sources/DependencyManagement.Core/Caches/DisposableCollection.cs
@@ -7,10 +7,21 @@ using Utils;
 /// <inheritdoc cref="DependencyManagement.Core.Caches.IDisposableCollection" />
 public class DisposableCollection : AsyncDisposableObject, IDisposableCollection
 {
-    private readonly HashSet<object> _disposables = new();
+    private readonly HashSet<object> _disposablesObjects = new();
+    
+    public DisposableCollection() { }
+    
+    /// <param name="items">Elements to add.</param>
+    public DisposableCollection(params object[] items)
+    {
+        foreach (var item in items)
+        {
+            Add(item);
+        }
+    }
 
     /// <inheritdoc />
-    public int Count => _disposables.Count;
+    public int Count => _disposablesObjects.Count;
 
     /// <inheritdoc />
     public bool IsReadOnly => false;
@@ -18,9 +29,32 @@ public class DisposableCollection : AsyncDisposableObject, IDisposableCollection
     /// <inheritdoc />
     public bool Add(object item)
     {
-        ThrowUtils.ThrowIfDisposed(IsDisposed);
+        Thrower.ThrowIfArgumentNull(item);
+        Thrower.ThrowIfObjectDisposed(IsDisposed);
+        
+        switch (item)
+        {
+            case IDisposableObject disposableObject:
+                disposableObject.Disposing += OnItemOnDisposing;
+                _disposablesObjects.Add(disposableObject);
+                break;
+            case IDisposable disposable:
+                _disposablesObjects.Add(disposable);
+                break;
+            case IAsyncDisposable asyncDisposable:
+                _disposablesObjects.Add(asyncDisposable);
+                break;
+        }
 
-        return _disposables.Add(item);
+        return false;
+    }
+
+    private void OnItemOnDisposing(object item)
+    {
+        if (item is not IDisposableObject disposableObject) return;
+        
+        disposableObject.Disposing -= OnItemOnDisposing;
+        _disposablesObjects.Remove(disposableObject);
     }
 
     void ICollection<object>.Add(object item)
@@ -31,31 +65,36 @@ public class DisposableCollection : AsyncDisposableObject, IDisposableCollection
     /// <inheritdoc />
     public void Clear()
     {
-        _disposables.Clear();
+        _disposablesObjects.Clear();
     }
 
     /// <inheritdoc />
     public bool Contains(object item)
     {
-        return _disposables.Contains(item);
+        return _disposablesObjects.Contains(item);
     }
 
     /// <inheritdoc />
     public void CopyTo(object[] array, int arrayIndex)
     {
-        _disposables.CopyTo(array, arrayIndex);
+        _disposablesObjects.CopyTo(array, arrayIndex);
     }
 
     /// <inheritdoc />
     public bool Remove(object item)
     {
-        return _disposables.Remove(item);
+        if (item is IDisposableObject disposableObject)
+        {
+            disposableObject.Disposing -= OnItemOnDisposing;
+        }
+
+        return _disposablesObjects.Remove(item);
     }
 
     /// <inheritdoc />
     public IEnumerator<object> GetEnumerator()
     {
-        return _disposables.GetEnumerator();
+        return _disposablesObjects.GetEnumerator();
     }
 
     IEnumerator IEnumerable.GetEnumerator()
@@ -68,21 +107,29 @@ public class DisposableCollection : AsyncDisposableObject, IDisposableCollection
     {
         if (disposing)
         {
-            foreach (var item in _disposables)
+            foreach (var item in _disposablesObjects)
             {
                 switch (item)
                 {
+                    case IDisposableObject and IDisposable disposableObject:
+                        ((IDisposableObject) disposableObject).Disposing -= OnItemOnDisposing;
+                        disposableObject.Dispose();
+                        break;
+                    case IDisposableObject and IAsyncDisposable asyncDisposableObject:
+                        ((IDisposableObject) asyncDisposableObject).Disposing -= OnItemOnDisposing;
+                        asyncDisposableObject.DisposeAsync().GetAwaiter().GetResult();
+                        break;
                     case IDisposable disposable:
                         disposable.Dispose();
                         break;
                     case IAsyncDisposable asyncDisposable:
-                        asyncDisposable.DisposeAsync().AsTask().Wait();
+                        asyncDisposable.DisposeAsync().GetAwaiter().GetResult();
                         break;
                 }
             }
         }
 
-        _disposables.Clear();
+        _disposablesObjects.Clear();
 
         base.DisposeCore(disposing);
     }
@@ -90,10 +137,18 @@ public class DisposableCollection : AsyncDisposableObject, IDisposableCollection
     /// <inheritdoc />
     protected override async ValueTask DisposeCoreAsync()
     {
-        foreach (var item in _disposables)
+        foreach (var item in _disposablesObjects)
         {
             switch (item)
             {
+                case IDisposableObject and IAsyncDisposable asyncDisposableObject:
+                    ((IDisposableObject) asyncDisposableObject).Disposing -= OnItemOnDisposing;
+                    await asyncDisposableObject.DisposeAsync();
+                    break;
+                case IDisposableObject and IDisposable disposableObject:
+                    ((IDisposableObject) disposableObject).Disposing -= OnItemOnDisposing;
+                    disposableObject.Dispose();
+                    break;
                 case IAsyncDisposable asyncDisposable:
                     await asyncDisposable.DisposeAsync();
                     break;
