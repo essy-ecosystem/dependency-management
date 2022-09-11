c4b3f84ab140fa4aceea76fa6dabc003dea252ce
Romana F <fembina@pm.me>
2022-09-11 19:52:02 +0300

    Remove async disposable object and add disposing delegate

--- Sources/DependencyManagement.Core/Disposables/IDisposableObject.cs
+++ Sources/DependencyManagement.Core/Disposables/IDisposableObject.cs
@@ -1,11 +1,18 @@
 namespace DependencyManagement.Core.Disposables;
 
+using Delegates;
+
 /// <summary>
-/// <inheritdoc cref="System.IDisposable" />
-/// Like <see cref="System.IDisposable" /> but with disposable status and events.
+/// Represents statues about the disposal of the object,
+/// and notifies about the disposal, before it.
 /// </summary>
-/// <inheritdoc cref="System.IDisposable" />
-public interface IDisposableObject : IDisposable
+/// <remarks>
+/// Recommended for use with standard
+/// <see cref="System.IDisposable"/>,or <see cref="System.IAsyncDisposable"/> interfaces.
+/// </remarks>
+/// <seealso cref="System.IDisposable" />
+/// <seealso cref="System.IAsyncDisposable" />
+public interface IDisposableObject
 {
     /// <summary>
     /// Gets a value indicating whether this instance is disposed.
@@ -22,5 +29,5 @@ public interface IDisposableObject : IDisposable
     /// <remarks>
     /// It should not be asynchronous.
     /// </remarks>
-    event Action<object> OnDisposing;
+    event DisposingDelegate Disposing;
 }
